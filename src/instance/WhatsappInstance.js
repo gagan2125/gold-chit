9d585ae54d10c028f096c74d1b674e9e40084223
grash2024 <grash2024@gmail.com>
2025-03-27 14:49:04 +0530

    url updated again with elastic ip

--- src/instance/WhatsappInstance.js
+++ src/instance/WhatsappInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const whatsappApi = axios.create({
-  baseURL: "http://16.171.198.197:3000/whatsapp/message",
+  baseURL: "http://13.51.87.99:3000/whatsapp/message",
   // baseURL: "http://localhost:3000/whatsapp/message",
  
 });
